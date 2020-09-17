b00777539fc5b3c2b454b281c25e0f5409d3a2cc
Zilin-Li <lynnlee829@gmail.com>
2020-09-17 23:11:05 +1200

    StartPage add

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <StartPage msg="OSSIS Worflow System"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import StartPage from '@/components/StartPage.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    StartPage
   }
 }
 </script>
